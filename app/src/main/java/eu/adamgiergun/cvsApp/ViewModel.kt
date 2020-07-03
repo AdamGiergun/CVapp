4ae73cc42b02fc62fc3da2a5d7acd12ce27c45c1
Adam Giergun <adam.giergun@gmail.com>
2020-07-03 16:18:59 +0200

    ver 1.1 Rewritten ViewModel.kt

--- app/src/main/java/eu/adamgiergun/cvsApp/ViewModel.kt
+++ app/src/main/java/eu/adamgiergun/cvsApp/ViewModel.kt
@@ -12,30 +12,23 @@ import kotlin.properties.Delegates
 class ViewModel(application: Application) : AndroidViewModel(application) {
     private var downloadId by Delegates.notNull<Long>()
 
-    fun startDownloadingCV() {
-        val downloadManager = getApplication<Application>().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        downloadId = downloadCV(downloadManager)
-    }
+    private fun getDownloadManager() = getApplication<Application>().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
 
-    private fun downloadCV(downloadManager: DownloadManager): Long {
+    fun startDownloadingCV() {
         val url =
             "https://gist.githubusercontent.com/AdamGiergun/10d8f582d6cca24b37eb4af396288bad/raw/896585ba9d125b17558e563f8805aa1c4120e434/cvs.xml"
         val downloadUri = Uri.parse(url)
-
         val request = DownloadManager.Request(downloadUri)
             .setAllowedOverMetered(true)
             .setAllowedOverRoaming(true)
             .setMimeType("application/xml")
 
-        val downloadId = downloadManager.enqueue(request)
+        val downloadManager = getDownloadManager()
+
+        downloadId = downloadManager.enqueue(request)
         val query = DownloadManager.Query().setFilterById(downloadId)
 
-        startDownloadInThread(downloadManager, query)
-        return downloadId
-    }
-
-    private fun startDownloadInThread(downloadManager: DownloadManager, query: DownloadManager.Query) {
-        Thread(Runnable {
+        val downloadCvThread = Thread(Runnable {
             var downloading = true
             while (downloading) {
                 val cursor: Cursor = downloadManager.query(query)
@@ -45,58 +38,71 @@ class ViewModel(application: Application) : AndroidViewModel(application) {
                 }
                 cursor.close()
             }
-        }).start()
+        })
+        downloadCvThread.start()
     }
 
     fun onBroadcastReceive(id: Long): CV {
+
+        fun getCvContent(): CV {
+
+            fun getCursor(): Cursor {
+                val query = DownloadManager.Query()
+                query.setFilterById(id)
+                return getDownloadManager().query(query)
+            }
+
+            val cursor = getCursor()
+
+            fun getCvContentFromCursor(): CV {
+                val cv = CV()
+                if (cursor.moveToFirst()) {
+                    val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                    if (status == DownloadManager.STATUS_SUCCESSFUL) {
+
+                        fun getParser(): XmlPullParser {
+                            val xmlFactoryObject = XmlPullParserFactory.newInstance()
+                            return xmlFactoryObject.newPullParser()
+                        }
+                        val myParser = getParser()
+
+                        fun getInputStream(): InputStream? {
+                            val localURI =
+                                Uri.parse(cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)))
+                            return getApplication<Application>().contentResolver.openInputStream(
+                                localURI
+                            )
+                        }
+                        myParser.setInput(getInputStream(), null)
+
+                        var event = myParser.eventType
+                        while (event != XmlPullParser.END_DOCUMENT) {
+                            when (event) {
+                                XmlPullParser.START_TAG -> {
+                                    cv.add(CvItem(true, myParser.name.replace("_", " ")))
+                                }
+                                XmlPullParser.TEXT -> {
+                                    cv.add(CvItem(false, myParser.text))
+                                }
+                            }
+                            event = myParser.next()
+                        }
+                    }
+                }
+                return cv
+            }
+
+            return getCvContentFromCursor()
+        }
+
         return if (downloadId == id) {
-            getCvContent(id)
+            getCvContent()
         } else {
-            CV(getApplication<Application>().getString(R.string.info_about_failed_download_of_cv))
-        }
-    }
-
-    private fun getCvContent(id: Long): CV {
-        val query = DownloadManager.Query()
-        query.setFilterById(id)
-        val downloadManager =
-            getApplication<Application>().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        return getCvContentFromCursor(downloadManager.query(query))
-    }
-
-    private fun getCvContentFromCursor(cursor: Cursor): CV {
-        var content = CV()
-        if (cursor.moveToFirst()) {
-            val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-            if (status == DownloadManager.STATUS_SUCCESSFUL) {
-                val localURI =
-                    Uri.parse(cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)))
-                val input: InputStream? =
-                    getApplication<Application>().contentResolver.openInputStream(localURI)
-                content = parseInputStream(input)
+            fun getInfoAboutFailure(): String {
+                val application = getApplication<Application>()
+                return application.getString(R.string.info_about_failed_download_of_cv)
             }
+            CV(getInfoAboutFailure())
         }
-        return content
-    }
-
-    private fun parseInputStream(input: InputStream?): CV {
-        val content = CV()
-        val xmlFactoryObject = XmlPullParserFactory.newInstance()
-        val myParser = xmlFactoryObject.newPullParser()
-        myParser.setInput(input, null)
-
-        var event = myParser.eventType
-        while (event != XmlPullParser.END_DOCUMENT) {
-            when (event) {
-                XmlPullParser.START_TAG -> {
-                    content.add(CvItem(true,myParser.name.replace("_", " ")))
-                }
-                XmlPullParser.TEXT -> {
-                    content.add(CvItem(false, myParser.text))
-                }
-            }
-            event = myParser.next()
-        }
-        return content
     }
 }
